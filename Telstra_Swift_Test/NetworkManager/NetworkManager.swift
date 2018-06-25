4358b6f408eee28dd56f6cf292485fc63c96f4ff
Mahesh Mavurapu <mahesh.m@techjini.com>
2018-06-25 22:55:10 +0530

    Added Utility class and Network Manager

--- Telstra_Swift_Test/NetworkManager/NetworkManager.swift
+++ Telstra_Swift_Test/NetworkManager/NetworkManager.swift
@@ -7,3 +7,61 @@
 //
 
 import Foundation
+
+typealias callbackHandler = (Error?, Any?, HTTPURLResponse?) -> Void
+
+class NetworkManager: NSObject {
+    
+    // Network Instance
+    class var sharedInstance: NetworkManager {
+        struct Singleton {
+            static let instance = NetworkManager()
+        }
+        return Singleton.instance
+    }
+    
+    // Get Request for List Details
+    func fetchDataFromServer(withUrl url: String?, andCompletionHandler completion: @escaping callbackHandler) {
+        if let anUrl = URL(string: url ?? "") {
+            let task = URLSession.shared.dataTask(with: anUrl) { (data, response, connectionError ) in
+                let httpResponse = response as? HTTPURLResponse
+                let error: Error? = nil
+                var iso: String? = nil
+                if let aData = data {
+                    iso = String(data: aData, encoding: .isoLatin1)
+                }
+                let dutf8: Data? = iso?.data(using: .utf8)
+                let json = try? JSONSerialization.jsonObject(with: dutf8!, options: []) as? NSDictionary
+                DispatchQueue.main.async(execute: {
+                    if json != nil {
+                        completion(nil, json as Any, httpResponse)
+                    } else {
+                        if let anError = error {
+                            print("error: \(anError)")
+                        }
+                        completion(nil, nil, httpResponse)
+                    }
+                })
+            }
+            task.resume()
+        }
+    }
+    
+    // Downloading Image
+    func downloadImageFromServer(with imgURL: URL?, andCompletionHandler completion: @escaping callbackHandler) {
+        if let anURL = imgURL {
+            let task = URLSession.shared.dataTask(with: anURL) { (data, response, connectionError ) in
+                let httpResponse = response as? HTTPURLResponse
+                if connectionError == nil {
+                    completion(nil, data, httpResponse)
+                } else {
+                    completion(connectionError, nil, httpResponse)
+                    if let anError = connectionError {
+                        print("\(anError)")
+                    }
+                }
+            }
+            task.resume()
+        }
+    }
+}
--- Telstra_Swift_Test/Utils/GlobalWidget.swift
+++ Telstra_Swift_Test/Utils/GlobalWidget.swift
@@ -7,3 +7,27 @@
 //
 
 import Foundation
+import UIKit
+
+typealias actionHandler = (UIAlertController) -> Void
+
+class GlobalWidget: NSObject {
+    
+    // Alert
+    class func alertMessage(_ message: String?, title: String?, andCompletionHandler completion: actionHandler) {
+        // Step-1
+        let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+        alertController.addAction(okAction)
+        // Step-2
+        var rootViewController = UIApplication.shared.delegate?.window??.rootViewController
+        if (rootViewController is UINavigationController) {
+            rootViewController = (rootViewController as? UINavigationController)?.viewControllers.first
+        }
+        if (rootViewController is UITabBarController) {
+            rootViewController = (rootViewController as? UITabBarController)?.selectedViewController
+        }
+        rootViewController?.present(alertController, animated: true)
+        completion(alertController)
+    }
+}
