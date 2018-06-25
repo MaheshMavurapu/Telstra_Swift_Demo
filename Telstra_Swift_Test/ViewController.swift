df0c85f285fc7ca3f3a31c360a152c1366040592
Mahesh Mavurapu <mahesh.m@techjini.com>
2018-06-25 17:41:50 +0530

    First Commit

---
+++ Telstra_Swift_Test/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Telstra_Swift_Test
+//
+//  Created by Mahesh Mavurapu on 25/06/18.
+//  Copyright © 2018 Mahesh Mavurapu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
