1e28d1a0e5afd82d0f75f2e3fd509352ddf40ac9
Mahesh Mavurapu <mahesh.m@techjini.com>
2018-06-25 19:19:06 +0530

    Added project class files

---
+++ Telstra_Swift_Test/ItemList/APICall/ItemListAPICall.swift
@@ -0,0 +1,9 @@
+//
+//  ItemListAPICall.swift
+//  Telstra_Swift_Test
+//
+//  Created by Mahesh Mavurapu on 25/06/18.
+//  Copyright © 2018 Mahesh Mavurapu. All rights reserved.
+//
+
+import Foundation
---
+++ Telstra_Swift_Test/ItemList/View/TableViewCell/ListTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ListTableViewCell.swift
+//  Telstra_Swift_Test
+//
+//  Created by Mahesh Mavurapu on 25/06/18.
+//  Copyright © 2018 Mahesh Mavurapu. All rights reserved.
+//
+
+import UIKit
+
+class ListTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
